d75779b2251bab846587e4345a62ce85c585085c
Corentin <corentin.neron@gmail.com>
2024-05-06 16:06:17 +0200

    code: windows compatible + linting + documentation

--- vsix_download.py
+++ vsix_download.py
@@ -6,9 +6,18 @@ Download the latest versions of all installed VSCode extensions with cURL.
 TODO: Update to allow for specifying extension version (but default to latest version).
 """
 
-import subprocess
 import argparse
+import platform
+import subprocess
 from pathlib import Path
+
+match platform.system():
+    case "Windows":
+        CURL_CMD = "curl.exe"
+    case "Linux":
+        CURL_CMD = "curl"
+    case _:
+        CURL_CMD = "CUSTOM_CURL_CMD"
 
 
 def vsix_url(extension: str):
@@ -18,6 +27,9 @@ def vsix_url(extension: str):
 
     ex: ms-python.python
 
+    Args:
+        extension (str): name of vscode extension "publisher.package"
+
     Returns:
         str: Url to the extension download
     """
@@ -25,15 +37,20 @@ def vsix_url(extension: str):
     return f'https://{publisher}.gallery.vsassets.io/_apis/public/gallery/publisher/{publisher}/extension/{package}/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage'
 
 
-def vsix_curl(extension: str, link: str, output_dir: str):
-    """
-    Builds and returns the cURL command to download a vscode extension
+def vsix_curl(extension: str, link: str, output_dir: str, curl_cmd: str = CURL_CMD):
+    """Builds and returns the cURL command to download a vscode extension
     to a spexified directory and filename.
+
+    Args:
+        extension (str): name of extension "publisher.package"
+        link (str): download link for the latest version of the extension
+        output_dir (str): output directory where the new .vsix will be stored
+        curl_cmd (str, optional): command to use for cURL, depends on OS. Defaults to CURL_CMD.
 
     Returns:
         str: Download command with curl
     """
-    return f'curl {link} -o {output_dir}/{extension}.vsix'
+    return f'{curl_cmd} {link} -o {output_dir}/{extension}.vsix'
 
 
 if __name__ == "__main__":
@@ -54,10 +71,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Arguments checks
-    if args.install:
-        INSTALL = True
-    else:
-        INSTALL = False
+    INSTALL = args.install
 
     if args.file:
         with open("extensions.txt", "r", encoding="utf-8") as file:
@@ -65,13 +79,13 @@ if __name__ == "__main__":
     else:
         # get a list of all installed extensions
         results = subprocess.run(
-            ['code', '--list-extensions'], stdout=subprocess.PIPE, check=True)
+            ['code', '--list-extensions'], stdout=subprocess.PIPE, check=True, shell=True)
         extensions = results.stdout.decode("utf-8").splitlines()
 
     # download each of the extensions and place them in a specified directory.
     for ext in extensions:
         url = vsix_url(ext)
         cmd = vsix_curl(ext, url, OUTPUT_DIR)
-        subprocess.run(cmd.split(), check=True)
+        subprocess.run(cmd.split(), check=True, shell=True)
         if True is INSTALL:
             subprocess.run(["code", "--install-extension", f"{OUTPUT_DIR}/{ext}.vsix"], check=True)
